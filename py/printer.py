46a2452684b1a4b9c8e3e05c0c55c18ebe5be51e
CamLav24 <121827092+CamLav24@users.noreply.github.com>
2026-01-22 00:03:27 -0600

    Implemented Url builder, update documentation, optimized printer.py to be call less functions

--- py/printer.py
+++ py/printer.py
@@ -1,16 +1,10 @@
 import os
 
-def write_out(f,Jobs_seen: str= "", job_title:str= "", location:str= "", job_id:str= ""):
-    f.write(f"\nJob {Jobs_seen}:")
-    f.write(f"  Title: {job_title.strip() if job_title else 'N/A'}")
-    f.write(f"  Location: {location.strip() if location else 'N/A'}")
-    f.write(f"  ID: {job_id.strip() if job_id else 'N/A'}")
+def write_out(f,Jobs_seen: str= "", job_title:str= "", location:str= "", job_id:str= "", url:str= ""):
+    f.write(f"\nJob {Jobs_seen}: Title:{job_title.strip() if job_title else 'N/A'}\nLocation:{location.strip() if location else 'N/A'}\nID:{job_id.strip() if job_id else 'N/A'}\nURL:{url if url else 'N/A'}")
     return
 
 
-def print_out(Jobs_seen: str= "", job_title:str= "", location:str= "", job_id:str= ""):
-    print(f"\nJob {Jobs_seen}:")
-    print(f"  Title: {job_title.strip() if job_title else 'N/A'}")
-    print(f"  Location: {location.strip() if location else 'N/A'}")
-    print(f"  ID: {job_id.strip() if job_id else 'N/A'}")
+def print_out(Jobs_seen: str= "", job_title:str= "", location:str= "", job_id:str= "",url:str= ""):
+    print(f"\nJob {Jobs_seen}: Title:{job_title.strip() if job_title else 'N/A'}\nLocation:{location.strip() if location else 'N/A'}\nID:{job_id.strip() if job_id else 'N/A'}\nURL:{url if url else 'N/A'}")
     return
--- py/web_core.py
+++ py/web_core.py
@@ -3,11 +3,10 @@ import random
 import time  
 from dotenv import load_dotenv
 from playwright.sync_api import sync_playwright
-
-from printer import print_out, write_out
+from Qual_checker import *
+from printer import *
 load_dotenv()
 website = os.getenv("website")
-
 
 def web_core(website: str = ""):
     Jobs_seen = 0
@@ -21,25 +20,22 @@ def web_core(website: str = ""):
             job_items = page.locator("li", has=page.locator("a[data-automation-id='jobTitle']")).all()
             for i, job in enumerate(job_items):
                 job_title = job.locator("a[data-automation-id='jobTitle']").text_content()
+                job_link = job.locator("a[data-automation-id='jobTitle']").get_attribute("href")
                 location = job.locator("div[data-automation-id='locations'] dd").text_content()
                 job_id = job.locator("ul[data-automation-id='subtitle'] li").text_content()
-                job.locator("a[data-automation-id='jobTitle']").click()
-                page.wait_for_selector("div[data-automation-id='jobPostingDescription']", timeout=5000)
-                job_description = job.locator("div[data-automation-id='jobPostingDescription']").text_content()
-                print(job_description)
                 Jobs_seen = Jobs_seen + 1
-                print_out(Jobs_seen, job_title, location, job_id)
-                write_out(f,Jobs_seen, job_title, location, job_id)
-                time.sleep(random.uniform(1, 2))  # Random sleep between 2 to 4 seconds
+                if check_qualifications(job_title) == False:
+                    continue
+                else:
+                    url = url_builder(job_link)
+                    print_out(Jobs_seen, job_title, location, job_id, url)
+                    write_out(f,Jobs_seen, job_title, location, job_id, url)
             next_button = page.locator("button[data-uxi-element-id='next']")
             if next_button.get_attribute("aria-disabled") == "true" or next_button.is_disabled():
-                print("\n✓ Reached the last page!")
                 break
-            print("\n→ Moving to next page...")
             next_button.click()
             page.wait_for_load_state("networkidle")
             time.sleep(random.uniform(2, 4))  # Random sleep between 2 to 4 seconds
-        input("\nPress Enter to close the browser...")
         browser.close()
         f.close()
     return
